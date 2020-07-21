5629dc9dd8c1232f97a5106e8007c54289e3a3f4
vandermnt <vanderson.mantovani@hotmail.com>
2020-07-21 11:37:13 -0300

    tela inicial

---
+++ site-infotech/resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Infotech - Soluções em Sistemas</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i%7CRajdhani:400,600,700" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/font-awesome/font-awesome.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+  <header class="menuTopBar">
+    <div class="container">
+      <div class="top-header d-none d-sm-flex justify-content-between align-items-center">
+        <div class="contact">
+        </div>
+        <nav class="d-flex aic" style="margin-top: ">
+          <a href="#" class="login"><i style="color: #00ffff" class="fa fa-user" aria-hidden="true"></i>Login</a>
+          <ul class="nav social d-none d-md-flex">
+            <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </nav>
+      </div>
+      <nav class="navbar navbar-expand-md navbar-light">
+        <a class="" href="#"><img id="logoInfo" style="" src="assets/images/logoinfo.png" alt="Infotech"></a>
+        <div class="group d-flex align-items-center">
+          <button class="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+          aria-expanded="false" aria-label="Toggle navigation"><span
+          class="navbar-toggler-icon"></span></button>
+          <a class="login-icon d-sm-none" href="#"><i class="fa fa-user"></i></a>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" style="margin-top:; margin-right: -32px">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" style="font-size: 17px" href="#"><b>Início</b></a></li>
+            <li class="nav-item"><a class="nav-link" style="font-size: 17px" href="#"><b>Empresa</b></a></li>
+            <li class="nav-item"><a class="nav-link" style="font-size: 17px" href="#"><b>Suporte Técnico</b></a></li>
+            <li class="nav-item"><a class="nav-link" style="font-size: 17px" href="#"><b>Produtos</b></a></li>
+            <li class="nav-item"><a class="nav-link" style="font-size: 17px" href="#"><b>Contato</b></a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <div class="hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 ">
+          <div class="swiper-container hero-slider">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide slide-content d-flex align-items-center">
+                <div>
+                  <b style="color: #00FFFF; margin-left: 5px; font-size: 35px">ÁGIL, SIMPLES E EFETIVO
+                  </b>
+                  <h3> Soluções Sob Medida  </h3>
+                  <h4> que funcionam para o seu negócio. </h4>
+                  <a style="margin-left: 5px; margin-top: 15px" class="btn btn-warning"> SAIBA MAIS </a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
